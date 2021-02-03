370e7d658320c479e20d8a3e354b7aba30234dbd
James Rezendes <jamesrezendes@Jamess-MacBook-Pro.local>
2021-02-03 13:22:23 -0800

    Added Prices

--- client/index.js
+++ client/index.js
@@ -56,49 +56,57 @@ const robots = {
     video : 'https://www.youtube.com/embed/86jfIiBGWqw',
     thumbnail : './assets/robot1.png',
     name : "Kiwi Robot",
-    description : "Kiwi delivers food and medical supplies to those around its area."
+    description : "Kiwi delivers food and medical supplies to those around its area.",
+    price: 800
   },
 
   two : {
     video : 'https://www.youtube.com/embed/dQWEOhXB70M',
     name : 'Miim',
-    description : "Miim is a feminine-looking humanoid robot created by the National Institute of Advanced Industrial Science and Technology"
+    description : "Miim is a feminine-looking humanoid robot created by the National Institute of Advanced Industrial Science and Technology",
+    price: 5000
   },
 
   three : {
     video : 'https://www.youtube.com/embed/f4sLFrprfbw',
     name : "Pepper",
-    description : "Pepper is a family-friendly robot that can help out around the house."
+    description : "Pepper is a family-friendly robot that can help out around the house.",
+    price: 2500
   },
 
   four : {
     video : 'https://www.youtube.com/embed/wOHYoe92jX0',
     name : "Asimo",
-    description : "Asimo is a cute, walking robot that would like to be your friend."
+    description : "Asimo is a cute, walking robot that would like to be your friend.",
+    price: 20000
   },
 
   five : {
     video : 'https://www.youtube.com/embed/zT-hhkvnmUw',
     name : "Sophia",
-    description : "Sophia is an advanced humanoid that wants humans and robots to be united."
+    description : "Sophia is an advanced humanoid that wants humans and robots to be united.",
+    price: 10000
   },
 
   six : {
     video : 'https://www.youtube.com/embed/wgzklGy4s8g',
     name : "Mark 1",
-    description : "A new and passionate relationship awaits for you and the Mark 1."
+    description : "A new and passionate relationship awaits for you and the Mark 1.",
+    price: 8000
   },
 
   seven : {
     video : 'https://www.youtube.com/embed/qyfxryvt9KU',
     name : "JiaJia",
-    description : "JiaJia is very happy to become your robot companion."
+    description : "JiaJia is very happy to become your robot companion.",
+    price: 7000
   },
 
   eight : {
     video : 'https://www.youtube.com/embed/C4SX4ycmlgA',
     name : "Titan",
-    description : "Titan is bound to keep you entertained for the future."
+    description : "Titan is bound to keep you entertained for the future.",
+    price: 15000
   }
 
 }
@@ -110,6 +118,7 @@ $('.featured').on('click', (e) => {
   $('.featuredVideo').attr('src', robot.video);
   $('.featuredName').text(robot.name);
   $('.featuredDesc').text(robot.description);
+  $('.buyPrice').text(`Selling at $${robot.price}`);
 })
 
 $('.featuredContainer').on('click', (e) => {
